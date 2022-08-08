cbd566c65478345de60cf7e12863b624223f2ec4
ri1ken <719401217@qq.com>
2022-08-09 04:50:42 +0900

    Rearrange blog style

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import 'nextra-theme-blog/style.css'
+import '../styles/main.css'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || ((page) => page);
+  return (
+    getLayout(<Component {...pageProps} />)
+  );
 }
--- theme.config.js
+++ theme.config.js
@@ -1,6 +1,38 @@
+const YEAR = new Date().getFullYear();
+
 export default {
-  footer: <p>MIT 2022 © Ri1ken.</p>,
-  head: ({ title, meta }) => (
+  footer: (
+    <div>
+      <hr />
+      <a href="https://twitter.com/Kiokh_" target="_blank">
+        Twitter
+      </a>{" "}
+      ·{" "}
+      <a href="https://github.com/Kiotlin" target="_blank">
+        GitHub
+      </a>{" "}
+      ·{" "}
+      <a href="mailto:r@iken.ri" target="_blank">
+        r@iken.ri
+      </a>
+      <small style={{ display: "block", marginTop: "8rem" }}>
+        <abbr
+          title="This site and all its content are licensed under a Creative Commons Attribution-NonCommercial 4.0 International License."
+          style={{ cursor: "help" }}
+        >
+          CC BY-NC 4.0
+        </abbr>{" "}
+        <time>{YEAR}</time> © ri1ken.
+        <a href="/feed.xml">RSS</a>
+        <style jsx>{`
+          a {
+            float: right;
+          }
+        `}</style>
+      </small>
+    </div>
+  ),
+  head: ({ meta }) => (
     <>
       {meta.description && <meta name="description" content={meta.description} />}
       {meta.tag && <meta name="keywords" content={meta.tag} />}
