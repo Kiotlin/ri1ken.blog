2b0ac1ab88a49226dc139d185d4f0a1a6a90c87c
ri1ken <719401217@qq.com>
2022-08-10 21:48:45 +0900

    js -> jsx

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import 'nextra-theme-blog/style.css'
-import '../styles/main.css'
-
-export default function Nextra({ Component, pageProps }) {
-  const getLayout = Component.getLayout || ((page) => page);
-  return (
-    getLayout(<Component {...pageProps} />)
-  );
-}
