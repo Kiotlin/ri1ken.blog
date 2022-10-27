533b667d6dc24e164b7a8b5a6aba8bfecf149004
ri1ken <719401217@qq.com>
2022-10-27 20:13:28 +0900

    add: vercel analysis

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,15 @@
 import 'nextra-theme-blog/style.css'
 import '../styles/main.css'
 
+import { Analytics } from '@vercel/analytics/react'
+
 export default function Nextra({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
 
-  return getLayout(<Component {...pageProps} />)
+  return (
+    <>
+      {getLayout(<Component {...pageProps} />)}
+      <Analytics />
+    </>
+  )
 }
